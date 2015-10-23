3fc33c65c27dc7b7c49fd23ee8ddc83228be0596
Soyeon <thdusdl1219@postech.ac.kr>
2015-10-23 02:23:34 -0700

    fixed #4 #5 I want to delicated exception handle in slave

--- master/src/main/scala/master/Main.scala
+++ master/src/main/scala/master/Main.scala
@@ -5,7 +5,7 @@ object Main {
   def main(args: Array[String]) = {
     val server = Master;
     // just example!
-    server.start(2)
+    server.start(args(0).toInt)
     server.slaveThread.foreach(_.join())
     server.ipAddrList foreach println
   }
--- slave/src/main/scala/slave/Main.scala
+++ slave/src/main/scala/slave/Main.scala
@@ -6,8 +6,11 @@ import java.nio._
 object Main {
   
   def main(args: Array[String]) = {
-
-    val client = new Slave("127.0.0.1:5959", "/data")
+    val oIndex = args.indexOf("-O")
+    val iIndex = args.indexOf("-I")
+    if(oIndex == -1 || iIndex == -1) throw new Exception("argument is strange")
+    val inputDir : Array[String] = args.slice(iIndex+1, oIndex) 
+    val client = new Slave(args(0), args(oIndex + 1))
     val buf = client.sendAndRecvOnce(ByteBuffer.wrap("Test".getBytes()))
     
 
