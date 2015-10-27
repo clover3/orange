117d37665ae1420f7c125cb72291338271233970
ywkim10 <ywkim10@postech.ac.kr>
2015-10-27 11:19:08 +0900

    fixes #2 fixes #4

--- slave/src/main/scala/slave/Main.scala
+++ slave/src/main/scala/slave/Main.scala
@@ -10,12 +10,8 @@ object Main {
     val iIndex = args.indexOf("-I")
     if(oIndex == -1 || iIndex == -1) throw new Exception("argument is strange")
     val inputDir : Array[String] = args.slice(iIndex+1, oIndex) 
-    val client = new Slave(args(0), args(oIndex + 1))
-    val buf = client.sendAndRecvOnce(ByteBuffer.wrap("Test".getBytes()))
-    
-
-    println(buf.get(0))
-    client.sock.close()
+    val slave = new Slave(args(0), inputDir.toList, args(oIndex + 1))
+    slave.run()
 
   }
 }
