f718e5a7e02565b9bbaec3c97a006f5723e0c369
ywkim10 <ywkim10@postech.ac.kr>
2015-11-02 12:11:29 +0900

    #18 : BigFile interface modification

--- slave/src/main/scala/slave/BigFile.scala
+++ slave/src/main/scala/slave/BigFile.scala
@@ -6,7 +6,9 @@ package slave
 
 import slave.Record._
 
-  trait IFile;
+import scala.concurrent.Future
+
+trait IFile;
 
   trait IBigFile {
     // returns total number of records in this file
@@ -17,24 +19,25 @@ import slave.Record._
 
     // return collection of records
     // starting from st to ed  ( it should not include ed'th record )
-    def getRecords(st: Int, ed: Int): Vector[Record]
+    def getRecords(st: Int, ed: Int): Future[Vector[Record]]
 
   }
 
-  // Delete abstract keyword after implementing BigFile
-  abstract class BigInputFile extends IBigFile {
-
+// Delete abstract keyword after implementing BigFile
+  class BigInputFile(inputDirs : List[String]) extends IBigFile {
     // generate BigFile from inputDirs
     // it reads all files in inputDirs and treat them as single file
-    def apply(inputDirs : List[String]) : IBigFile = ???
+    def numOfRecords: Int = ???
+
+  // get i'th record
+    def getRecord(i: Int): Record = ???
+
+  // return collection of records
+  // starting from st to ed  ( it should not include ed'th record )
+    def getRecords(st: Int, ed: Int): Future[Vector[Record]] = ???
   }
 
   // Delete abstract keyword after implementing BigFile
-  abstract class BigOutputFile extends IBigFile {
-    def setRecords(records : Vector[Record])
-    // generate BigFile to save outputFile
-    // it makes files inside outputDir, and the file names starts with namePrefix
-    // if outputDir = "Out" , namePrefix = "file",
-    // it will generates "Out/file1", "Out/file2"... etc
-    def apply(outputDir : String, namePrefix : String ) : IBigFile = ???
+  abstract class BigOutputFile(outputPath: String) extends IBigFile {
+    def setRecords(records : Vector[Record]) : Future[Unit]
   }
