907b52b13309c0fbc43bb3ddea744f9a2b1d6a96
beomseok0203 <beomseok0203@postech.ac.kr>
2015-11-02 18:28:13 +0900

    #19 ResourceChecker : iResourceChecker

--- slave/src/main/scala/slave/ResourceChecker.scala
+++ slave/src/main/scala/slave/ResourceChecker.scala
@@ -4,19 +4,22 @@ package slave
  * Created by Clover on 2015-10-31.
  */
 trait IResourceChecker {
+//memory info
   val mb = 1024 * 1024
+  val runtime = Runtime.getRuntime
 
   // returns the memory that this program is using
-  def usedMemory:Int
+  def usedMemory:Int = ((runtime.totalMemory - runtime.freeMemory())/ mb).toInt
+ //??? can i use int ? or Long type.
 
   // returns the remaining memory that this program can use
-  def remainingMemory:Int = totalMemory - usedMemory
+  def remainingMemory:Int = (runtime.freeMemory / mb).toInt
 
   // returns the total memory that this program can use
-  def totalMemory:Int
+  def totalMemory:Int = (runtime.totalMemory()).toInt
 }
 
-// delete abstract after implementing methods
+// delete abstract after implementing methods - ???
 abstract class ResourceChecker extends IResourceChecker
 {
 
