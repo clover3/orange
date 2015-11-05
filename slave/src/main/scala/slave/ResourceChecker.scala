4c94c63739b2af1483514ecae860e27eefd502da
ywkim10 <ywkim10@postech.ac.kr>
2015-11-06 01:17:48 +0900

    #19 : we do not need to divide by mb

--- slave/src/main/scala/slave/ResourceChecker.scala
+++ slave/src/main/scala/slave/ResourceChecker.scala
@@ -9,16 +9,16 @@ class ResourceChecker {
   val runtime = Runtime.getRuntime
 
   // returns the memory that this program is using
-  def usedMemory:Int = ((runtime.totalMemory - runtime.freeMemory())/ mb).toInt
+  def usedMemory:Int = (runtime.totalMemory - runtime.freeMemory()).toInt
  //??? can i use int ? or Long type.
 
   // returns the remaining memory that this program can use
-  def remainingMemory:Int = (runtime.freeMemory / mb).toInt
+  def remainingMemory:Int = runtime.freeMemory.toInt
 
   // returns the total memory that this program can use
-  def totalMemory:Int = (runtime.totalMemory()).toInt
+  def totalMemory:Int = runtime.totalMemory().toInt
 
   // returns the Max Memory
-  def maxMemory : Int = (runtime.maxMemory() / mb).toInt
+  def maxMemory : Int = runtime.maxMemory().toInt
 }
 
