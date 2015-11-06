39785471cbf2ae6e923593f0b8b12ac2d6977292
beomseok0203 <beomseok0203@postech.ac.kr>
2015-11-06 16:48:01 +0900

    #18 IBigFile
    implement getrecord(i)

--- slave/src/main/scala/slave/BigFile.scala
+++ slave/src/main/scala/slave/BigFile.scala
@@ -4,14 +4,12 @@ package slave
  * Created by Clover on 2015-10-30.
  */
 
-import java.io.{FileNotFoundException, File}
+import java.io.{File, FileNotFoundException, RandomAccessFile}
 
-import common.typedef._
 import slave.Record._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.io.Source
 
 
 trait IBigFile {
@@ -27,11 +25,12 @@ trait IBigFile {
 
 }
 
-// Delete abstract keyword after implementing BigFile
-class BigInputFile(inputDirs : List[String]) extends IBigFile {
+
+// I add argument inputDirs(:List[String])
+class BigInputFile(inputDirs : List[String])  extends IBigFile{
 
   ///////////////////////////////////
-  ///file Info
+  ///file Info(dir)
   def getFileList(dirPath: String) : List[File] ={
     val d = new File(dirPath)
     if (d.exists && d.isDirectory)
@@ -44,55 +43,44 @@ class BigInputFile(inputDirs : List[String]) extends IBigFile {
 
   //for access to wanted index
   val totalFileNum :Int = fileList.length
-////////////////////////////////////////////////
+  ////////////////////////////////////////////////
 
-    // generate BigFile from inputDirs
-    // it reads all files in inputDirs and treat them as single file
-  def numOfRecords: Int = {
-      //fileList.map(numOfRecordsEachFile).foldLeft(0)(_+_)
-      fileList.map(numOfRecordsEachFile).sum //(fileList.map(numOfRecordsEachFile):List[Int]).sum
-
-      def numOfRecordsEachFile(file: File) :Int = {
-        var sum :Int =0 ///sry for using var...T_T
-        for (line <- Source.fromFile(file).getLines()) {
-          sum = sum +1
-        }
-        sum
-      }
-
-//      def numOfRecordsEachFile(file: File) :Int = {
-//        val sum = for (line <- Source.fromFile(file).getLines()) yield {
-//        } sum
-//      }
-    1 // just for compile..
-    }
+  // returns total number of records in this file
+  def numOfRecords: Int = 327680
 
   // get i'th record
-  def getRecord(i: Int): Record = ???
+  def getRecord(i: Int): Record = {
+    //define randomAccessFile just for read("r)
+    val raf = new RandomAccessFile(fileList.head, "r")
+
+    //set Offset for key or value
+    //ex) AsfAGHM5om  00000000000000000000000000000000  0000222200002222000022220000222200002222000000001111
+    //    10 - 32 - 52
+    val keyOffset :Long = 10
+    val valueOffset1 :Long = 32
+    val valueOffset2 :Long = 52
+    //num 2 blank for each blank !?!?
+    val totalOffset :Long = keyOffset + 2 + valueOffset1 + 2 + valueOffset2
+
+    //set position
+    var pos :Long = (totalOffset) * i
+    raf.seek(pos)
+
+    //val keyVal = raf.readLine().take(keyOffset.toInt)
+    val keyString = raf.readLine().take(keyOffset.toInt)
+    val dataString = raf.readLine().drop(keyOffset.toInt)
+    (keyString, dataString)
+  }
 
   // return collection of records
   // starting from st to ed  ( it should not include ed'th record )
-  def getRecords(st: Int, ed: Int): Vector[Record] = {
-    // 1.read file from inputDir -> 2.make fileList ->3. get Record -> 4.make Records
-
-    //     // var ListRecordEachFile : vector[Record]
-    //      def vectorRecordEachFile(file: File) : Vector[Record]  = {
-    //       for (line <- Source.fromFile(file).getLines()) yield {
-    //         new Record(line.take(10), line.drop(10)) // parsing File as " "
-    //       }
-    //       vectorRecordEachFile(file).toVector
-    //     }
-    //
-    ////all file vector
-    //    val vectorRecordAllFile : Vector[Record] = fileList.flatMap(file=>vectorRecordEachFile(file)).toVector
-    //
-    //    // from start index to end index
-    //    val indexedVectorRecord : Vector[Record] = vectorRecordAllFile.slice(st,ed)
-    //
-    //  }
-    ??? // just for compile...
+  def getRecords(st: Int, ed: Int): Vector[Record] =
+  {
+  
+      val seq = for (i <- Range(st, ed)) yield getRecord(i)
+      seq.toVector
+    
   }
-
 
 }
 
@@ -112,10 +100,10 @@ class ConstFile extends IBigFile{
   // starting from st to ed  ( it should not include ed'th record )
   def getRecords(st: Int, ed: Int): Vector[Record] =
   {
-  
-      val seq = for (i <- Range(st, ed)) yield getRecord(i)
-      seq.toVector
-    
+
+    val seq = for (i <- Range(st, ed)) yield getRecord(i)
+    seq.toVector
+
   }
 
 }
@@ -133,7 +121,7 @@ class NullOutputFile extends IOutputFile {
 
   }
 
-  def toInputFile : IBigFile = new ConstFile
+  def toInputFile : IBigFile = new ConstFile(Nil)
 
 }
 
