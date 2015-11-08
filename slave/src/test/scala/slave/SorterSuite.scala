f46d59fff2336f77d060161b0594510c070fae0e
ywkim10 <ywkim10@postech.ac.kr>
2015-11-08 21:28:48 +0900

    #25

---
+++ slave/src/test/scala/slave/SorterSuite.scala
@@ -0,0 +1,22 @@
+package slave
+
+import org.scalatest.FunSuite
+import slave.SlaveSorter._
+
+/**
+ * Created by Clover on 2015-11-06.
+ */
+class SorterSuite extends FunSuite {
+
+  test("sorting test - virtual file") {
+    val input: IBigFile = new ConstFile
+    val rs:ResourceChecker = new ResourceChecker()
+    val merger: ChunkMerger = new SingleThreadMerger
+    val sorter = new SingleThreadSorter(rs)
+
+    // operate on
+    val sortedChunks: List[IBigFile] = sorter.generateSortedChunks(input)
+//    merger.MergeBigChunk(sortedChunks)
+  }
+
+}
